d51f813535f277012de0a17277a1022ff1c16604
Joseph LaForgia <laforgiaj@gmail.com>
2020-05-25 00:21:25 -0400

    initial db commit

---
+++ db.js
@@ -0,0 +1,10 @@
+const { Client } = require('pg')
+
+const client = new Client({
+  user: 'postgres',
+  host: 'counter-1.c4fxt0ukb7r1.us-east-2.rds.amazonaws.com',
+  database: 'counter',
+  password: 'FLOPjelop22',
+  port: 5432,
+})
+client.connect()
